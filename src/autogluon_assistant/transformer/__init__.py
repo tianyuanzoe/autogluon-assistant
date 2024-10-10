9a1f36c2d81ce29c1eb409ae1f1e0fafd00ababd
Haoyang Fang <107515844+FANGAreNotGnu@users.noreply.github.com>
2024-10-10 09:53:41 -0700

    [Fix] Remove Previous Task Inference and Deleted Code (#32)

--- src/autogluon_assistant/transformer/__init__.py
+++ src/autogluon_assistant/transformer/__init__.py
@@ -1,25 +1,11 @@
 from .base import BaseTransformer, TransformTimeoutError
 from .feature_transformers import BaseFeatureTransformer, CAAFETransformer, OpenFETransformer, PretrainedEmbeddingTransformer
-from .task_inference import (
-    EvalMetricInferenceTransformer,
-    FilenameInferenceTransformer,
-    LabelColumnInferenceTransformer,
-    ProblemTypeInferenceTransformer,
-    TestIdColumnTransformer,
-    TrainIdColumnDropTransformer,
-)
 
 __all__ = [
     "BaseTransformer",
     "BaseFeatureTransformer",
     "CAAFETransformer",
     "PretrainedEmbeddingTransformer",
-    "EvalMetricInferenceTransformer",
-    "FilenameInferenceTransformer",
-    "LabelColumnInferenceTransformer",
-    "ProblemTypeInferenceTransformer",
     "OpenFETransformer",
-    "TestIdColumnTransformer",
-    "TrainIdColumnDropTransformer",
     "TransformTimeoutError",
 ]
