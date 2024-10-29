a90702ed5716aefa0bf9039fe105facd025aac84
Haoyang Fang <107515844+FANGAreNotGnu@users.noreply.github.com>
2024-10-29 14:39:40 -0700

    Fix error on sonnet(new) (#60)

--- src/autogluon_assistant/prompting/prompt_generator.py
+++ src/autogluon_assistant/prompting/prompt_generator.py
@@ -48,6 +48,7 @@ class PromptGenerator(ABC):
             "1. Return only valid JSON. No extra explanations, text, or comments.\n"
             "2. Ensure that the output can be parsed by a JSON parser directly.\n"
             "3. Do not include any non-JSON text or formatting outside the JSON object."
+            "4. An example is \{\"<provided_field>\": \"<correct_value_for_the_field>\"\}"
         )
 
     def generate_chat_prompt(self):
