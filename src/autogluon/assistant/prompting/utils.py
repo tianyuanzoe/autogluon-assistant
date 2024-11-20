507eaa4b729fc31a3b7679d966474840e3c21b04
Anirudh Dagar <anirudhdagar6@gmail.com>
2024-11-20 10:38:47 +0100

    BUG: Avoid generating duplicates in column_names (#156)

--- src/autogluon/assistant/prompting/utils.py
+++ src/autogluon/assistant/prompting/utils.py
@@ -1,2 +1,4 @@
 def get_outer_columns(all_columns, num_columns_each_end=10):
+    if len(all_columns) <= num_columns_each_end * 2:
+        return list(all_columns)
     return all_columns[:num_columns_each_end] + all_columns[-num_columns_each_end:]
