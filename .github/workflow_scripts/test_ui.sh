15a50eeafe279b68888f92f6a16f148b8e8e17f5
YUAN TIAN <tiyuano@amazon.com>
2024-12-19 22:50:18 +0000

    change coverage file location

--- .github/workflow_scripts/test_ui.sh
+++ .github/workflow_scripts/test_ui.sh
@@ -4,4 +4,4 @@ set -ex
 source $(dirname "$0")/env_setup.sh
 
 install_ui_test
-python3 -m pytest --junitxml=ui_results.xml tests/unittests/ui --cov-report json:ui_coverage.json
+python3 -m pytest --junitxml=ui_results.xml tests/unittests/ui --cov-report json:./ui_coverage.json
